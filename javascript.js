9a04c530dd48635e9bfafce43d6a65cb55726a84
haidy <haidy.duenas@gmail.com>
2017-07-19 12:16:37 -0700

    new

---
+++ javascript.js
@@ -0,0 +1,9 @@
+function crearTablita(){
+	var asientos = 32
+	var cajita = "";
+	for (var i = 0; i = < asientos; i++) {
+		var cajita += "<tr><th>Asientos</th><th>Savings</th></tr>"
+	
+	}
+	cajita.innerHt
+}
